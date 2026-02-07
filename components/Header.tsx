0e3a01f14e2ea9f8d5a93378b105eb2b5b6db0de
malaythakur <malaythakur13@gmail.com>
2026-02-07 18:29:57 +0530

    feat: add header component

---
+++ components/Header.tsx
@@ -0,0 +1,23 @@
+import Link from 'next/link';
+import Image from 'next/image';
+import NavItems from "@/components/NavItems";
+import UserDropdown from "@/components/UserDropdown";
+
+const Header = () => {
+    return (
+        <header className="sticky top-0 header">
+            <div className="container header-wrapper">
+                <Link href="/">
+                    <Image src="/assets/icons/logo.svg" alt="La Bolsa logo" width={5140} height={62} className="h-8 w-auto cursor-pointer" />
+                </Link>
+
+                <nav className="hidden sm:block">
+                    <NavItems />
+                </nav>
+
+                <UserDropdown />
+            </div>
+        </header>
+    )
+}
+export default Header
